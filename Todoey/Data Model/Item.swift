f6181d7b98fd25e1c6a1e0130d996cc7125eaf8c
DeAndre Jones <drej08@yahoo.com>
2018-04-17 22:07:33 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Apis on 4/12/18.
+//  Copyright © 2018 Apis. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
